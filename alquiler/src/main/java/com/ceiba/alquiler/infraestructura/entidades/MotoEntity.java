48053ea4727460b78b357881000670552c298ed7
Daniel Castro Jara - Ceiba Software <daniel.castro@ceiba.com.co>
2019-11-19 20:43:46 -0500

    Mapping with postegrs and adding driver

---
+++ alquiler/src/main/java/com/ceiba/alquiler/infraestructura/entidades/MotoEntity.java
@@ -0,0 +1,84 @@
+package com.ceiba.alquiler.infraestructura.entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class MotoEntity implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name = "placa")
+	private String placa;
+	
+	@Column(name = "marca")
+	private String marca;
+	
+	@Column(name = "anio_modelo")
+	private int anioModelo;
+	
+	@Column(name = "referencia")
+	private String referencia;
+	
+	@Column(name = "cilindrada")
+	private int cilindrada;
+	
+	@Column(name = "precio_alquiler")
+	private int precioAlquiler;
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public int getAnioModelo() {
+		return anioModelo;
+	}
+
+	public void setAnioModelo(int anioModelo) {
+		this.anioModelo = anioModelo;
+	}
+
+	public String getReferencia() {
+		return referencia;
+	}
+
+	public void setReferencia(String referencia) {
+		this.referencia = referencia;
+	}
+
+	public int getCilindrada() {
+		return cilindrada;
+	}
+
+	public void setCilindrada(int cilindrada) {
+		this.cilindrada = cilindrada;
+	}
+
+	public int getPrecioAlquiler() {
+		return precioAlquiler;
+	}
+
+	public void setPrecioAlquiler(int precioAlquiler) {
+		this.precioAlquiler = precioAlquiler;
+	}
+
+}
