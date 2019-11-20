9d9d8d66d7d0dbcf574fcb7918c3046a5b019c04
Daniel Castro Jara - Ceiba Software <daniel.castro@ceiba.com.co>
2019-11-19 19:43:03 -0500

    Changing test notation

--- alquiler/src/test/java/com/ceiba/alquiler/AlquilerApplicationTests.java
+++ alquiler/src/test/java/com/ceiba/alquiler/AlquilerApplicationTests.java
@@ -3,7 +3,7 @@ package com.ceiba.alquiler;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class AlquilerApplicationTests {
 
 	@Test
