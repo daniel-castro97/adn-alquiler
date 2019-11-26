a48e62a4fe3ab8c60f9dc6a46dbf48013e6136e8
Daniel Castro Jara - Ceiba Software <daniel.castro@ceiba.com.co>
2019-11-26 10:26:13 -0500

    Adding first test to moto comando

---
+++ alquiler/src/test/java/com/ceiba/alquiler/dataBuilder/ComandoMotoTestDatabuilder.java
@@ -0,0 +1,75 @@
+package com.ceiba.alquiler.dataBuilder;
+
+import java.util.Date;
+
+import com.ceiba.alquiler.aplicacion.comando.ComandoMoto;
+
+public class ComandoMotoTestDatabuilder {
+	private String placa;
+	private String marca;
+	private int anioModelo;
+	private String referencia;
+	private int cilindrada;
+	private int precioAlquiler;
+	
+	public ComandoMotoTestDatabuilder (){
+		this.placa= "rhd79e";
+		this.marca = "pulsar";
+		this.anioModelo =2018;
+		this.referencia = "NS160";
+		this.cilindrada = 160;
+		this.precioAlquiler = 20000;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public int getAnioModelo() {
+		return anioModelo;
+	}
+
+	public void setAnioModelo(int anioModelo) {
+		this.anioModelo = anioModelo;
+	}
+
+	public String getReferencia() {
+		return referencia;
+	}
+
+	public void setReferencia(String referencia) {
+		this.referencia = referencia;
+	}
+
+	public int getCilindrada() {
+		return cilindrada;
+	}
+
+	public void setCilindrada(int cilindrada) {
+		this.cilindrada = cilindrada;
+	}
+
+	public int getPrecioAlquiler() {
+		return precioAlquiler;
+	}
+
+	public void setPrecioAlquiler(int precioAlquiler) {
+		this.precioAlquiler = precioAlquiler;
+	}
+	
+	public ComandoMoto build() {
+		return new ComandoMoto(placa, marca, anioModelo, referencia, cilindrada, precioAlquiler);
+	}
+}
--- alquiler/src/test/java/com/ceiba/alquiler/integracion/MotoControllerTest.java
+++ alquiler/src/test/java/com/ceiba/alquiler/integracion/MotoControllerTest.java
@@ -1,17 +1,23 @@
 package com.ceiba.alquiler.integracion;
 
 import org.junit.Before;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.ceiba.alquiler.AlquilerApplication;
+import com.ceiba.alquiler.aplicacion.comando.ComandoMoto;
+import com.ceiba.alquiler.dataBuilder.ComandoMotoTestDatabuilder;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
@@ -32,6 +38,12 @@ public class MotoControllerTest {
 	@Before
 	public void setup() throws Exception {
 	this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+	}
+	@Test
+	public void crear() throws Exception{
+		ComandoMoto moto =  new ComandoMotoTestDatabuilder().build();
+		mockMvc.perform(post("/lista/crear").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(moto))).andExpect(status().isOk());
 	}
 
 }
